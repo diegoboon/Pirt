4c343e71fcf31f438227cd468e41bfeb25506193
unknown <xin.dmoreno@gmail.com>
2020-07-16 11:29:58 -0500

    comentario1

---
+++ src/config/api.js
@@ -0,0 +1,10 @@
+export const API = 'https://boon0.com/propuestas/tienda/';
+
+export const CONSUMER_KEY = 'ck_796c2beba29b32d4d96b887f3687a5ea184c4084';
+export const CONSUMER_SECRET = 'cs_9fd9635cb6bc87f3d6c568a4491d61f1feb1cdce';
+
+export default {
+  API_ENDPOINT: API,
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+};
